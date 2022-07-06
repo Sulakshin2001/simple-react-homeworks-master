86268d259c5110d5cd9af53ae66350ebfa0137de
Sulakshin2001 <nikitasulaaskin@gmail.com>
2022-07-06 18:08:17 +0300

    Updates

---
+++ precache-manifest.77082d8a517bbe31ee04e10dd199079b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a30597de3a8852a411ae8802fdf692e1",
+    "url": "/simple-react-homeworks-master-2-0/index.html"
+  },
+  {
+    "revision": "6d6ac8dabac29cf2dadb",
+    "url": "/simple-react-homeworks-master-2-0/static/css/main.976528ea.chunk.css"
+  },
+  {
+    "revision": "5109d3e4d9ee4819410d",
+    "url": "/simple-react-homeworks-master-2-0/static/js/2.c73c9df9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/simple-react-homeworks-master-2-0/static/js/2.c73c9df9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d6ac8dabac29cf2dadb",
+    "url": "/simple-react-homeworks-master-2-0/static/js/main.1d808c4b.chunk.js"
+  },
+  {
+    "revision": "de380480e2ef73252a8c",
+    "url": "/simple-react-homeworks-master-2-0/static/js/runtime-main.066ec17d.js"
+  }
+]);
